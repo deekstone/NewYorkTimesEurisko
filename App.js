2ee51943dc7164b73839012f8c1c8884c13b434e
softmind <elie.deek@softmind.com.lb>
2020-09-16 17:37:26 +0300

    change views folder name

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { enableScreens } from 'react-native-screens';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import newsReducer from './src/redux/reducers/NewsReducer';
-import { StackViewsApp } from './src/Views/StackScreens';
+import { enableScreens } from "react-native-screens";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import newsReducer from "./src/redux/reducers/NewsReducer";
+import { StackViewsApp } from "./src/nav/StackScreens";
 enableScreens();
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
@@ -17,15 +17,15 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
  * of the Redux store. This is handy for a variety of tasks, such as
  * expressing asynchronous actions in a concise manner, or logging every
  * action payload.
- * 
+ *
  **/
 const store = createStoreWithMiddleware(newsReducer);
 export default function App() {
-	return (
-		<Provider store={store}>
-			<NavigationContainer>
-				<StackViewsApp />
-			</NavigationContainer>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <StackViewsApp />
+      </NavigationContainer>
+    </Provider>
+  );
 }
